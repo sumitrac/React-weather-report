bbcce4e51000c0439d13a9ad23fd77032891577c
Sumitra <sumitrac2008@gmail.com>
2021-06-24 17:54:54 -0700

    added change colors

--- src/component/Temperature.js
+++ src/component/Temperature.js
@@ -8,42 +8,45 @@ const Temperature = () => {
     // const [tempColor, setTempColor] =useState('')
 
     const tempIncrease = () => {
-        if (tempCount > 75) 
-        alert('It is hot outside.')
+        if (tempCount + 1 > 75) {
+            alert('It is hot outside.');
+        }
         tempSet(tempCount + 1); 
         // backgroundColor()
 
     };
 
     const tempDecrease = () => {
-        if (tempCount < 32) 
-            alert('It is Freezing temperature!')
+        if (tempCount - 1 < 32) {
+            alert('It is Freezing temperature!');
+        }
             tempSet(tempCount - 1);
             // setTempColor(backgroundColor())
     };
 
-    // const backgroundColor = () => {
-    //     if (tempCount > 80) {
-    //         return 'red';
-    //     }else if (tempCount > 70) {
-    //         return 'orange'; 
-    //     }else if (tempCount > 60) {
-    //         return 'yellow';
-    //     }else if (tempCount > 50) {
-    //         return 'green';
-    //     }else {
-    //         return 'teal'; 
-    //     };
-    // };
+    const changeColor = () => {
+        if (tempCount > 80) {
+            return 'red';
+        }else if (tempCount > 70) {
+            return 'orange'; 
+        }else if (tempCount > 60) {
+            return 'yellow';
+        }else if (tempCount > 50) {
+            return 'green';
+        }else {
+            return 'teal'; 
+        };
+    };
 
     // const backgroundColor = isBackgroundRed ? 'green' : 'red';
     // return (
     //     <div className={isBackgroundRed ? 'red': 'orange'} />
     // )
+    const backgroundColor = changeColor();
 
     return (
         <div className='main_div'>
-            <div className='center-div'>
+            <div className={`center-div ${backgroundColor}`}>
                 <h2>{tempCount} °F</h2>
                 <div className='btn_div'>
                     <button onClick={tempIncrease}>UP</button>
